3cff4dd8f538788bc5c1aeb7ad5ab90c19b0fb1f
Maciej Rutkowski <dev@nomadic.works>
2014-10-07 09:55:22 +0200

    Initial commit

---
+++ article.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
+
+	"github.com/russross/blackfriday"
+)
+
+type PageType int
+
+const (
+	Post PageType = iota
+	Page
+	Snippet
+)
+
+type Article struct {
+	Author       string
+	DateModified *time.Time
+	DateUpdated  *time.Time
+	Title        string
+	Content      string
+	Description  string
+	Filename     string
+	Link         string
+	Identifier   string
+	Snippet      bool
+	Type         PageType
+	Draft        bool
+	Tags         []string
+}
+
+func (a Article) HasTag(aTag string) bool {
+	for _, tag := range a.Tags {
+		if aTag == tag {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (a Article) BasePath() string {
+	switch a.Type {
+	case Post, Snippet:
+		return path.Join(strconv.Itoa(a.DateModified.Year()), fmt.Sprintf("%02d", int(a.DateModified.Month())))
+	case Page:
+		return ""
+	}
+
+	return ""
+}
+
+func (a Article) FullPath() string {
+	return path.Join(a.BasePath(), a.Filename)
+}
+
+type Articles []*Article
+
+func (a Articles) Len() int {
+	return len(a)
+}
+
+func (a Articles) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a Articles) Less(i, j int) bool {
+	left := *(a[i].DateModified)
+	right := *(a[j].DateModified)
+
+	return right.Before(left)
+}
+
+func ReadArticle(reader *bufio.Reader) Article {
+	article := Article{}
+
+	line, lineErr := reader.ReadString('\n')
+
+	article.Type = Post
+
+	for lineErr == nil {
+
+		if strings.HasPrefix(line, "---") {
+			break
+		}
+
+		values := strings.SplitN(line, ":", 2)
+
+		if len(values) < 2 {
+			line, lineErr = reader.ReadString('\n')
+			continue
+		}
+
+		key, value := values[0], values[1]
+
+		key = strings.Trim(key, " \t\r\n")
+		value = strings.Trim(value, " \t\r\n")
+
+		switch key {
+		case "title":
+			article.Title = value
+		case "author":
+			article.Author = value
+		case "description":
+			article.Description = value
+		case "link":
+			article.Link = value
+		case "date":
+			dateStr := value
+			modTime, timeErr := time.Parse(time.RFC3339, dateStr)
+			if timeErr == nil {
+				article.DateModified = &modTime
+			} else {
+				log.Fatalf("Could not parse date \"%s\"", dateStr)
+			}
+
+		case "updated":
+			dateStr := value
+			modTime, timeErr := time.Parse(time.RFC3339, dateStr)
+			if timeErr == nil {
+				article.DateUpdated = &modTime
+			} else {
+				log.Fatalf("Could not parse date \"%s\"", dateStr)
+			}
+		case "type":
+			switch value {
+			case "Post":
+				article.Type = Post
+			case "Page":
+				article.Type = Page
+			case "Snippet":
+				article.Type = Snippet
+			}
+
+		case "tags":
+			fieldsFunc := func(divider rune) bool {
+				return unicode.IsSpace(divider) || divider == ',' || divider == ';'
+			}
+
+			for _, tag := range strings.FieldsFunc(value, fieldsFunc) {
+				article.Tags = append(article.Tags, strings.ToLower(tag))
+			}
+		}
+
+		line, lineErr = reader.ReadString('\n')
+	}
+
+	contentBuffer := bytes.NewBufferString("")
+	line, lineErr = reader.ReadString('\n')
+
+	for lineErr == nil {
+		contentBuffer.WriteString(line)
+		contentBuffer.WriteString("\n")
+
+		line, lineErr = reader.ReadString('\n')
+	}
+
+	htmlFlags := 0
+	// htmlFlags |= blackfriday.HTML_USE_XHTML
+	htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_FRACTIONS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
+	// htmlFlags |= blackfriday.HTML_SANITIZE_OUTPUT
+	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
+	extensions := 0
+	extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+	extensions |= blackfriday.EXTENSION_TABLES
+	extensions |= blackfriday.EXTENSION_FENCED_CODE
+	extensions |= blackfriday.EXTENSION_AUTOLINK
+	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
+	extensions |= blackfriday.EXTENSION_SPACE_HEADERS
+	extensions |= blackfriday.EXTENSION_HEADER_IDS
+
+	md := blackfriday.Markdown(contentBuffer.Bytes(), renderer, extensions)
+
+	article.Content = string(md)
+
+	if len(article.Description) == 0 {
+		article.Description = article.Content
+	}
+
+	return article
+}
---
+++ blogger.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"sort"
+	"strings"
+	"text/template"
+	"time"
+)
+
+var blogTitle = flag.String("title", "blog", "Blog title")
+var destinationExt = flag.String("extension", "", "Destination file extension")
+var sourcePath = flag.String("source", "source", "Source directory")
+var destinationPath = flag.String("destination", "destination", "Destination directory")
+var siteRoot = flag.String("root", "/", "Site root path")
+
+const templateFileName = "templates/template.html"
+const rssTemplateFileName = "templates/rsstemplate.html"
+
+func main() {
+
+	flag.Parse()
+
+	log.Printf("Blog title is %s", *blogTitle)
+
+	destinationDir, destinationDirErr := os.Open(*destinationPath)
+
+	if destinationDirErr != nil {
+		log.Fatal("Destination directory could not be open", destinationDirErr)
+	}
+
+	defer destinationDir.Close()
+
+	funcMap := template.FuncMap{
+		"longDate":     func(args ...interface{}) string { return args[0].(*time.Time).Format("Monday, _2 January 2006, 15:04") },
+		"snippetDate":  func(args ...interface{}) string { return args[0].(*time.Time).Format("Jan _2 2006, 15:04") },
+		"shortDate":    func(args ...interface{}) string { return args[0].(*time.Time).Format("Jan _2, 2006") },
+		"atomDate":     func(args ...interface{}) string { return args[0].(*time.Time).Format("2006-01-02T15:04:05Z07:00") },
+		"Snippet":      func(args ...interface{}) bool { return args[0].(*Article).Type == Snippet },
+		"last":         func(index, count int) bool { return index == count-1 },
+		"tagIndexName": func(tag string) string { return "tag-" + tag + *destinationExt },
+		"path": func(article Article) string {
+			return article.FullPath()
+		},
+	}
+
+	mainTemplate := template.Must(template.New("template.html").Funcs(funcMap).ParseFiles(path.Join(*sourcePath, templateFileName)))
+	mainRssTemplate := template.Must(template.New("rsstemplate.html").Funcs(funcMap).ParseFiles(path.Join(*sourcePath, rssTemplateFileName)))
+
+	now := time.Now()
+
+	postDir := path.Join(*sourcePath, "posts")
+
+	sourceFiles, sourceFilesErr := ioutil.ReadDir(postDir)
+	if sourceFilesErr != nil {
+		log.Fatal("Source directory could not be read")
+	}
+
+	var articles Articles
+
+	var indexArticles Articles
+	var feedArticles Articles
+	var snippetArticles Articles
+
+	for _, sourceFileInfo := range sourceFiles {
+
+		ext := path.Ext(sourceFileInfo.Name())
+
+		if sourceFileInfo.IsDir() || (ext != ".markdown" && ext != ".md") {
+			continue
+		}
+
+		name := strings.TrimSuffix(path.Base(sourceFileInfo.Name()), ext)
+
+		sourceFile, _ := ioutil.ReadFile(path.Join(postDir, sourceFileInfo.Name()))
+
+		sourceBuffer := bytes.NewBuffer(sourceFile)
+
+		mdReader := bufio.NewReader(sourceBuffer)
+
+		line, lineErr := mdReader.ReadString('\n')
+
+		if lineErr != nil {
+			continue
+		}
+
+		var article Article
+
+		if strings.HasPrefix(line, "---") {
+			article = ReadArticle(mdReader)
+			article.Filename = name + *destinationExt
+		} else {
+			log.Fatal("Bad article")
+			continue
+		}
+
+		if article.DateModified == nil {
+			article.DateModified = new(time.Time)
+			*article.DateModified = sourceFileInfo.ModTime()
+		}
+
+		article.Identifier = name
+
+		if strings.HasSuffix(name, "draft") {
+			article.Draft = true
+		}
+
+		articles = append(articles, &article)
+
+		if article.Type == Page {
+			continue
+		}
+
+		if article.Draft {
+			continue
+		}
+
+		if article.Type == Post {
+			feedArticles = append(feedArticles, &article)
+		}
+
+		if article.Type == Snippet {
+			snippetArticles = append(snippetArticles, &article)
+		}
+
+		indexArticles = append(indexArticles, &article)
+	}
+
+	tags := map[string]bool{}
+
+	sort.Sort(articles)
+	sort.Sort(indexArticles)
+	sort.Sort(feedArticles)
+	sort.Sort(snippetArticles)
+
+	indexBuffer := bytes.NewBufferString("")
+	rssIndexBuffer := bytes.NewBufferString("")
+	snippetrssIndexBuffer := bytes.NewBufferString("")
+
+	mainTemplate.Execute(indexBuffer, map[string]interface{}{"Title": blogTitle, "Home": true, "Root": siteRoot, "Articles": indexArticles, "CreatedTime": now})
+	mainRssTemplate.Execute(rssIndexBuffer, map[string]interface{}{"Title": blogTitle, "Home": true, "Root": siteRoot, "Articles": feedArticles, "CreatedTime": &now})
+	mainRssTemplate.Execute(snippetrssIndexBuffer, map[string]interface{}{"Title": blogTitle, "Home": true, "Root": siteRoot, "Articles": snippetArticles, "CreatedTime": &now})
+
+	for _, article := range articles {
+
+		destFileBuffer := bytes.NewBufferString("")
+
+		mainTemplate.Execute(destFileBuffer, map[string]interface{}{"BlogTitle": blogTitle, "Article": article, "Title": string(article.Title + " – " + *blogTitle), "Home": false, "Root": siteRoot})
+
+		for _, tag := range article.Tags {
+			tags[tag] = true
+		}
+
+		destinationFileName := path.Join(destinationDir.Name(), article.FullPath())
+
+		os.MkdirAll(path.Join(destinationDir.Name(), article.BasePath()), os.ModePerm)
+
+		writeErr := ioutil.WriteFile(destinationFileName, destFileBuffer.Bytes(), os.ModePerm)
+
+		if writeErr != nil {
+			log.Println(writeErr)
+		}
+	}
+
+	indexFileName := path.Join(destinationDir.Name(), "index.html")
+	rssIndexFileName := path.Join(destinationDir.Name(), "index.xml")
+	snippetIndexFileName := path.Join(destinationDir.Name(), "snippets.xml")
+
+	ioutil.WriteFile(indexFileName, indexBuffer.Bytes(), os.ModePerm)
+	ioutil.WriteFile(rssIndexFileName, rssIndexBuffer.Bytes(), os.ModePerm)
+	ioutil.WriteFile(snippetIndexFileName, snippetrssIndexBuffer.Bytes(), os.ModePerm)
+
+	for tag := range tags {
+
+		tagIndexBuffer := bytes.NewBufferString("")
+		var tagArticles Articles
+
+		for _, article := range indexArticles {
+
+			if !article.HasTag(tag) {
+				continue
+			}
+
+			tagArticles = append(tagArticles, article)
+		}
+
+		mainTemplate.Execute(tagIndexBuffer, map[string]interface{}{"Articles": tagArticles, "Title": "Tag: " + tag + " – " + *blogTitle, "Home": false, "Root": siteRoot})
+
+		tagIndexFileName := path.Join(destinationDir.Name(), "tag-"+tag+*destinationExt)
+		ioutil.WriteFile(tagIndexFileName, tagIndexBuffer.Bytes(), os.ModePerm)
+	}
+}
