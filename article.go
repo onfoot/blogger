2cbd65a904adb1a8c11456789e383a84d32629a6
Maciej Rutkowski <dev@nomadic.works>
2016-01-04 16:55:25 +0100

    Added some documentation Fixed a not yet encountered bug of skipping front matter lines on line parse error

--- article.go
+++ article.go
@@ -15,19 +15,26 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+// PageType is a convenience type alias for page type
 type PageType string
 
 const (
-	Post    PageType = "Post"
-	Page             = "Page"
-	Snippet          = "Snippet"
+	// Post - A regular blog article
+	Post PageType = "Post"
+	// Page - A site-wide page
+	Page = "Page"
+	// Snippet - Twitter-like short blog post
+	Snippet = "Snippet"
 )
 
 const (
+	// DefaultDateFormat is the default format used in posts and templates
 	DefaultDateFormat string = time.RFC3339
-	IFTTTDateFormat          = "January 02, 2006 at 03:04PM"
+	// IFTTTDateFormat is a date format IFTTT uses for recipe ingredients in actions
+	IFTTTDateFormat = "January 02, 2006 at 03:04PM"
 )
 
+// Article represents a blogger post, page or a snippet. Contains information useful for blog publishing.
 type Article struct {
 	Author       string
 	DateModified *time.Time
@@ -47,6 +54,7 @@ type Article struct {
 	Meta         map[string]string
 }
 
+// HasTag checks if the given article contains a certain tag
 func (a Article) HasTag(aTag string) bool {
 	for _, tag := range a.Tags {
 		if aTag == tag {
@@ -57,9 +65,14 @@ func (a Article) HasTag(aTag string) bool {
 	return false
 }
 
+// BasePath returns a base path for the given article, relative to blog root path
 func (a Article) BasePath() string {
 	switch a.Type {
 	case Post, Snippet:
+		if a.Draft {
+			return "drafts"
+		}
+
 		return path.Join(strconv.Itoa(a.DateModified.Year()), fmt.Sprintf("%02d", int(a.DateModified.Month())))
 	case Page:
 		return ""
@@ -68,10 +81,12 @@ func (a Article) BasePath() string {
 	return ""
 }
 
+// FullPath combines BasePath with articles file name
 func (a Article) FullPath() string {
 	return path.Join(a.BasePath(), a.Filename)
 }
 
+// Articles is an convenience type alias for article slice
 type Articles []*Article
 
 func (a Articles) Len() int {
@@ -89,6 +104,7 @@ func (a Articles) Less(i, j int) bool {
 	return right.Before(left)
 }
 
+// Print sends an article header in plain text to standard output
 func (a Article) Print() {
 	fmt.Println("---")
 
@@ -130,6 +146,7 @@ func (a Article) Print() {
 	fmt.Println("")
 }
 
+// ParseFrontMatter reads the front matter-type article header
 func ParseFrontMatter(reader *bufio.Reader) (map[string]string, error) {
 
 	data := make(map[string]string)
@@ -140,10 +157,10 @@ func ParseFrontMatter(reader *bufio.Reader) (map[string]string, error) {
 		return data, errors.New("Invalid front matter header")
 	}
 
-	for lineErr == nil {
+	for {
 		line, lineErr = reader.ReadString('\n')
 		if lineErr != nil {
-			continue
+			break
 		}
 
 		if strings.HasPrefix(line, "---") {
@@ -153,7 +170,6 @@ func ParseFrontMatter(reader *bufio.Reader) (map[string]string, error) {
 		values := strings.SplitN(line, ":", 2)
 
 		if len(values) < 2 {
-			line, lineErr = reader.ReadString('\n')
 			continue
 		}
 
@@ -168,6 +184,7 @@ func ParseFrontMatter(reader *bufio.Reader) (map[string]string, error) {
 	return data, nil
 }
 
+// ReadArticle returns an article read from a Reader
 func ReadArticle(reader *bufio.Reader) (Article, error) {
 	article := Article{}
 
@@ -254,7 +271,6 @@ func ReadArticle(reader *bufio.Reader) (Article, error) {
 				article.Tags = append(article.Tags, strings.ToLower(tag))
 			}
 		}
-
 	}
 
 	if article.DateModified == nil {
